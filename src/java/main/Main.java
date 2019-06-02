a8ee5e58b51e8ed358e36803ad539073c8aabad7
bpiotrowski1 <baej.p26@gmail.com>
2019-06-02 14:24:16 +0200

    final commit

--- src/java/main/Main.java
+++ src/java/main/Main.java
@@ -1,9 +1,40 @@
+import java.util.Scanner;
+
 public class Main {
-    public static void main(String[] args) {
-        for (int i = 0; i < 1000; i++) {
-            if (i==2 || i==3 || (i % 2 != 0 && i % 3 != 0)) {
-                System.out.print(i + " ");
+    final static private int[] primes = {127, 131, 137, 139, 149};
+
+    public static void main(String[] args) throws java.lang.Exception {
+        Scanner scanInt = new Scanner(System.in);
+        int n = scanInt.nextInt();
+
+        for (int i = 0; i < n; i++) {
+            int k = scanInt.nextInt();
+            int[] letters = new int[k];
+            for (int j = 0; j < k; j++) {
+                letters[j] = scanInt.nextInt();
+            }
+
+            int prime = getPrime(letters[letters.length/2]);
+            if (prime == -1) {
+                System.out.println("NIECZYTELNE");
+            } else {
+                System.out.print(prime + " ");
+                for (int j = 0; j < k; j++) {
+                    int result = letters[j] % prime;
+                    System.out.print((char) result);
+                }
+                System.out.println();
             }
         }
+    }
+
+    private static int getPrime(int letter){
+        for (int i = primes.length - 1; i >= 0; i--) {
+            int result = letter % primes[i];
+            if (result >= 65 && result <= 90) {
+                return primes[i];
+            }
+        }
+        return -1;
     }
 }
